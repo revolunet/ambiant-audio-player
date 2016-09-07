173aa1f6e6c2072d4ea245117daf42baa4d94409
Julien Bouquillon <julien@revolunet.com>
2016-09-07 12:21:05 +0200

    better sync

--- sync.sh
+++ sync.sh
@@ -3,17 +3,28 @@
 cd `dirname "$0"`
 
 FILES="requirements.txt player.py start.sh pixel.gif"
-DEST_PATH="/root/simpleplayer/"
 
-function sync() {
+sync_rpi() {
   TARGET=$1
+  DEST_PATH="/root/simpleplayer/"
   ssh $TARGET ipe-rw
   rsync -v $FILES $TARGET:$DEST_PATH
-  scp rc.local $TARGET:/etc/rc.local
+  #scp rc.local $TARGET:/etc/rc.local
+  ssh $TARGET pip install cherrypy
   ssh $TARGET ipe-ro
+  ssh $TARGET reboot
 }
 
-sync $1
+
+sync_ubuntu() {
+  TARGET=$1
+  DEST_PATH="/home/ju/simpleplayer"
+  rsync -v $FILES $TARGET:$DEST_PATH
+ # ssh $TARGET pip install cherrypy
+ # ssh $TARGET sudo reboot
+}
+
+sync_rpi $1
 
 
 
