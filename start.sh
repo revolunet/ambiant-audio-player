0e42fd140f96da6dc3b73fe87f5f4420e2db33ad
Julien Bouquillon <contact@revolunet.com>
2017-09-27 18:00:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 cd `dirname "$0"`
 
-HOSTNAME=`hostname`
+IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 
-URL="http://192.168.10.10:9500/getloop?hostname=$HOSTNAME"
+URL="http://192.168.10.10:9500/getloop?ip=$IP"
 
 echo "GETTING LOOP FROM $URL"
 
